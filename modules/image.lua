7dd95fc7eb527fc0e6b087473770e2be866f65e0
Cuppy130 <player130games@gmail.com>
2025-03-18 12:52:52 -0700

    fixed image.lua

--- modules/image.lua
+++ modules/image.lua
@@ -3,20 +3,19 @@ Image = {}
 function Image.new(path, x, y, width, height)
     local self = {}
     self.path = path
-    self.x = x
-    self.y = y
-    self.width = width
-    self.height = height
+    self.x = x or 0
+    self.y = y or 0
+
     self.image = love.graphics.newImage(path)
-    self.image:setFilter("nearest", "nearest")
-    self.scaleX = width / self.image:getWidth()
-    self.scaleY = height / self.image:getHeight()
+    local imgWidth, imgHeight = self.image:getDimensions()
+    self.width = width or imgWidth
+    self.height = height or imgHeight
+    self.scaleX = self.width / imgWidth
+    self.scaleY = self.height / imgHeight
 
     function self.draw()
         love.graphics.draw(self.image, self.x, self.y, 0, self.scaleX, self.scaleY)
     end
 
-    function self.update()
-        -- Update logic if needed
-    end
+    return self
 end
